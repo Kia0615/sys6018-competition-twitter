3f338731b162195ebce994494dea528b569d5d84
Duncan Rule <wrule1@babson.edu>
2017-09-18 01:37:15 -0400

    fix mode

--- KNN.R
+++ KNN.R
@@ -1,6 +1,10 @@
 #KNN function that takes a training set with a rightmost response column 
 #takes a single row of a test set and returns the predicted factor level
 #to be applied to each row of a test set
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
 
 myKNN = function(train, testVal, k) {
   distances = data.frame(matrix(nrow=nrow(train), ncol = length(testVal)))
@@ -11,9 +15,7 @@ myKNN = function(train, testVal, k) {
   trainComp = cbind(train, squares)
   trainComp = trainComp[order(squares),]
   trainComp = trainComp[1:k,]
-  trainComp = trainComp[order(trainComp[, length(trainComp)-1])]
-  return(trainComp$response[1])
+  print(trainComp)
+  return(Mode(trainComp$response))
 }
-
-
 
